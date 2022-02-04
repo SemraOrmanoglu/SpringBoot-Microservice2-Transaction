c1ebd17b1787e7791838ea04d7f667789575472a
OGM\semraormanoglu <semraormanoglu@ogm.gov.tr>
2022-02-04 14:40:08 +0300

    initial commit

---
+++ src/main/java/com/sha/transaction/controller/TransactionController.java
@@ -0,0 +1,45 @@
+package com.sha.transaction.controller;
+
+import com.sha.transaction.model.entity.Transaction;
+import com.sha.transaction.model.service.AbstractTransactionService;
+import com.sha.transaction.model.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/transaction")
+@RestController
+public class TransactionController
+{
+    @Autowired
+   private AbstractTransactionService service;
+
+    @GetMapping("{userID}")
+    public ResponseEntity<List<Transaction>> getTransactionOfUser(@PathVariable Integer userID)
+    {
+       return ResponseEntity.ok(service.findAllTransactionsOfUser(userID));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Transaction> saveTransaction(@RequestBody Transaction transaction)
+    {
+        Transaction savedTransaction=service.save(transaction);
+        return new ResponseEntity<>(savedTransaction, HttpStatus.CREATED);
+
+    }
+    @DeleteMapping("{transactionID}")
+    public ResponseEntity<?> deleteTransactionByID(@PathVariable Integer transactionID)
+    {
+        service.deleteByID(transactionID);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransaction()
+    {
+       return ResponseEntity.ok(service.findAll());
+    }
+}
